99db9dfea11f8af7d276f80fe157d1177d2270a3
CamelUrine <peterlastimosa494@gmail.com>
2023-11-20 08:16:14 +0800

    11-20

--- components/videolinks.js
+++ components/videolinks.js
@@ -49,6 +49,71 @@ const videoLinks = [
     src:
       'https://drive.google.com/file/d/1hJY9o6TJKtxEFGHoTUwkKU2SLbBXJi07/preview',
   },
+  {
+    name: 'Episode 11 - Muhammad Meets The Joker ',
+    src:
+      'https://drive.google.com/file/d/1QIdt9c88qky1tQc2RwsC07c8SCC1YL45/preview',
+  },
+  {
+    name: 'Episode 12 - Muhammad Meets Apostate Prophet ',
+    src:
+      'https://drive.google.com/file/d/1FVfDX73DWg5WN0BgeObpp42X19cICQVt/preview',
+  },
+  {
+    name: 'Episode 13 - Muhammad Meets Klansman',
+    src:
+      'https://drive.google.com/file/d/1b7EAFwbLOvOJ6ywcdeBXnVtlQtv4hQJ3/preview',
+  },
+  {
+    name: 'Episode 14 - Muhammad Meets Youtube Trust & Safety',
+    src:
+      'https://drive.google.com/file/d/1nRivgZ6lP5NAoeX0WfCUdN6P4kFcCyZC/preview',
+  },
+  {
+    name: 'Episode 15 - Muhammad Meets Moses',
+    src:
+      'https://drive.google.com/file/d/1JiSkWPy_QKkWw-7s6-kdxl94AFsu5zum/preview',
+  },
+  {
+    name: 'Episode 16 - Muhammad Meets Darth Vader',
+    src:
+      'https://drive.google.com/file/d/14kWcqtHZIPpBXcrZk05MeFpa6yxIiIP1/preview',
+  },
+  {
+    name: 'Episode 17 - Muhammad Meets Santa Claus',
+    src:
+      'https://drive.google.com/file/d/1EmceCXmZ32mPehvjhEzX_7wU5UjEN0wX/preview',
+  },
+  {
+    name: 'Episode 18 - Muhammad Meets Jeffrey Epstein',
+    src:
+      'https://drive.google.com/file/d/1xfQ4cB2pF9hHMqnglLJnUo3FlaQuNXLY/preview',
+  },
+  {
+    name: 'Episode 19 - Muhammad Meets Angel Gabriel',
+    src:
+      'https://drive.google.com/file/d/1YeDR7b1LvtB5IgkepLQdqjDa9OZou5xH/preview',
+  },
+  {
+    name: 'Episode 20 - Muhammad Meets Sigmund Freud',
+    src:
+      'https://drive.google.com/file/d/1iFgTeGKFwzRq0hzk6VI_iSZtxXQK2USC/preview',
+  },
+  {
+    name: 'Episode 21 - Muhammad Meets Hatun Tash',
+    src:
+      'https://drive.google.com/file/d/13p6gCpP54oY_Hs7wvSgPDnJT31q5nhYB/preview',
+  },
+  {
+    name: 'Episode 22 - Muhammad Meets Elon Musk',
+    src:
+      'https://drive.google.com/file/d/17CEsiLPBHTgSGkSUFWrQ3SIwAYuTY8ci/preview',
+  },
+  {
+    name: 'Episode 23 - Muhammad Meets Andrew Tate',
+    src:
+      'https://drive.google.com/file/d/1kS9mykwt62z0nhGdX-71Pe3Cf2jikb0y/preview',
+  },
 ]
 
 export default videoLinks
--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,11 @@ import { useState } from 'react'
 
 const Home = () => {
   const [currentVideo, setCurrentVideo] = useState(videoLinks[0])
-
+  //<div className="px-3 scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-slate-200  overflow-y-auto h-full lg:h-[39rem] gap-2 flex flex-col">
   return (
     <>
       <Head>
-        <title>{`Muhammad's Boom Boom Room1`}</title>
+        <title>{`Muhammad's Boom Boom Room TV`}</title>
         <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700&display=swap"
@@ -18,7 +18,7 @@ const Home = () => {
       <div className="w-full h-screen bg-[#23251F]">
         <div className="w-inherit p-7 border-b-2">
           <p className="text-3xl text-white font-bold">
-            {`Muhammad's Boom Boom Room`}
+            {`Muhammad's Boom Boom Room TV`}
           </p>
         </div>
         <div className="flex gap-x-6 flex-row w-full px-8 py-10">
@@ -26,7 +26,8 @@ const Home = () => {
             <iframe
               className="mb-6 w-full border-2 bg-slate-700 border-slate-700 h-[30rem]"
               src={currentVideo.src}
-              allow="autoplay"
+              allow="autoplay; encrypted-media"
+              allowFullScreen
             ></iframe>
             <p className="text-white text-2xl font-semibold">
               {currentVideo.name}
@@ -34,9 +35,9 @@ const Home = () => {
           </div>
           <div className="w-2/5">
             <p className="text-[#a4dc9a] text-2xl font-semibold p-2">
-              Episode lists
+              Episode list
             </p>
-            <div className="w-inherit flex items-start flex-col">
+            <div className="w-inherit h-[500px] overflow-y-scroll flex items-start flex-col">
               {videoLinks.map((item, key) => (
                 <button
                   onClick={() => setCurrentVideo(item)}
